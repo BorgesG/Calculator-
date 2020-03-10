3de2df7508815ed1b1d7b1c103967c7f9ae88105
Gabriel Borges <gabrielborges3140@gmail.com>
2020-03-10 13:04:44 -0300

    My First App

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,41 @@
+import 'package:calculator/models/memory.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../components/display.dart';
+import '../components/keyboard.dart';
+
+
+class Calculator extends StatefulWidget {
+
+
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+_onPressed(String command){
+  setState(() {
+    memory.applyCommando(command);
+  });
+}
+
+  @override
+  Widget build(BuildContext context){
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
+
+    return MaterialApp(
+      home:Column(
+        children: [
+          Display(memory.value),
+          Keyboard(_onPressed),   
+        ],
+      )
+    );
+  }
+}
